e8fa8427dd0ccb47b27e326d558770f7a894c022
Daniel Samuel <dsamuel@student.42wolfsburg.de>
2025-05-02 10:57:41 +0200

    Initial file & project setup modules

---
+++ includes/Webserv.hpp
@@ -0,0 +1,12 @@
+#ifndef WEBSERV_HPP
+#define WEBSERV_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <fstream>
+#include <unistd.h>
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "includes/Webserv.hpp"
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+    std::cout << "Webserv starting..." << std::endl;
+    return 0;
+}
